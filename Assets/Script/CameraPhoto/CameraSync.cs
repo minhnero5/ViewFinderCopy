7e5e23dafa51e6b945fa75668eaae149cd3c67dd
minhnero5 <minhnero5@gmail.com>
2024-07-25 17:46:16 +0700

    update

---
+++ Assets/Script/CameraPhoto/CameraSync.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraSync : MonoBehaviour
+{
+    public Camera mainCamera; // Camera chính
+    public Camera secondaryCamera; // Camera thứ hai
+
+    void LateUpdate()
+    {
+        // Đồng bộ hóa vị trí và xoay
+        secondaryCamera.transform.position = mainCamera.transform.position;
+        secondaryCamera.transform.rotation = mainCamera.transform.rotation;
+
+        // Đồng bộ hóa Field of View (FOV)
+        secondaryCamera.fieldOfView = mainCamera.fieldOfView;
+    }
+}
